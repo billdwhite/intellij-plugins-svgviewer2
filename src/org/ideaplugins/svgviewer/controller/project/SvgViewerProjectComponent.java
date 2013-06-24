6a73b57279d0833928218851adf38f950e3e3d5b
Vladislav Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2013-06-24 08:25:34 +0400

    Remove deprecated methods usage

--- src/org/ideaplugins/svgviewer/controller/project/SvgViewerProjectComponent.java
+++ src/org/ideaplugins/svgviewer/controller/project/SvgViewerProjectComponent.java
@@ -1,11 +1,15 @@
 package org.ideaplugins.svgviewer.controller.project;
 
 import com.intellij.openapi.components.ProjectComponent;
-import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import com.intellij.util.messages.MessageBusConnection;
 import org.ideaplugins.svgviewer.Helpers;
 import org.ideaplugins.svgviewer.controller.application.Configuration;
 import org.ideaplugins.svgviewer.view.SvgViewerPanel;
@@ -18,10 +22,9 @@ implements ProjectComponent {
     public static final String ID_TOOL_WINDOW = "SvgViewer";
     private Project _project;
     private FileListener _fileListener;
-    private EditorListener _editorListener;
     private SvgViewerPanel _viewerPanel;
     private static final String PROJECT_COMPONENT_NAME = "ProjectComponent";
-
+    private MessageBusConnection _connection;
 
 
     public SvgViewerProjectComponent(Project project) {
@@ -65,26 +68,28 @@ implements ProjectComponent {
         this._viewerPanel = new SvgViewerPanel();
 
         ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(this._project);
-        ToolWindow toolWindow = toolWindowManager.registerToolWindow(SvgViewerProjectComponent.ID_TOOL_WINDOW, getViewerPanel(), ToolWindowAnchor.RIGHT);
-
+        ToolWindow toolWindow = toolWindowManager.registerToolWindow(SvgViewerProjectComponent.ID_TOOL_WINDOW, false, ToolWindowAnchor.RIGHT);
+        final Content content = ContentFactory.SERVICE.getInstance().createContent(getViewerPanel(), "", false);
+        content.setCloseable(false);
+        toolWindow.getContentManager().addContent(content);
         toolWindow.setIcon(Helpers.ICON_TOOL_WINDOW);
         getViewerPanel().getCanvas().setToolWindow(toolWindow);
 
-        this._editorListener = new EditorListener(getViewerPanel());
-        FileEditorManager.getInstance(this._project).addFileEditorManagerListener(this._editorListener);
+        this._connection = this._project.getMessageBus().connect();
+        this._connection.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new EditorListener(getViewerPanel()));
 
         this._fileListener = new FileListener(getViewerPanel());
-        this._project.getProjectFile().getFileSystem().addVirtualFileListener(this._fileListener);
+        VirtualFileManager.getInstance().addVirtualFileListener(this._fileListener);
     }
 
 
 
     public void unregisterToolWindow() {
-        if (this._editorListener != null) {
-            FileEditorManager.getInstance(this._project).removeFileEditorManagerListener(this._editorListener);
+        if (this._connection != null) {
+            this._connection.disconnect();
         }
         if (this._fileListener != null) {
-            this._project.getProjectFile().getFileSystem().removeVirtualFileListener(this._fileListener);
+            VirtualFileManager.getInstance().removeVirtualFileListener(this._fileListener);
         }
         if (isToolWindowRegistered()) {
             ToolWindowManager.getInstance(this._project).unregisterToolWindow(SvgViewerProjectComponent.ID_TOOL_WINDOW);
